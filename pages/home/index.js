a15bdc89b0390e60a33d5729e1fbdd28333bd95f
Antonio <netoifpe@gmail.com>
2022-09-22 17:25:22 -0300

    first commmit

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,2 +1,10 @@
 /* Desenvolva a lógica da página aqui */
+const controllersModal = document.querySelectorAll("[data-control-modal]")
 
+
+for(let index = 0; index < controllersModal.length; index++){
+    controllersModal[index].addEventListener("click", ()=>{
+        let modalId = controllersModal[index].getAttribute("data-control-modal")
+        document.getElementById(modalId).classList.toggle("show-modal")
+    })
+}
