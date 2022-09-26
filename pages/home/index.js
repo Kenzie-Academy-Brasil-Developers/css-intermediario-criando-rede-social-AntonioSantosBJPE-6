4f7593bf162b8631d1c55845bb6314d812c7fc16
Antonio <netoifpe@gmail.com>
2022-09-26 11:12:59 -0300

    refinando o texto dos cards de post

--- pages/home/index.js
+++ pages/home/index.js
@@ -82,7 +82,7 @@ function createCardPost (idFather, idProfile, idPost , positionPostArray){
             liCardPost.appendChild(h2)
 
             let p = document.createElement("p")
-            p.innerText = posts[positionPostArray].text
+            p.innerHTML = posts[positionPostArray].text
             p.classList = "font-text1 txt-color-grey-2 text-align-justify posts-text"
             liCardPost.appendChild(p)
 
